673cc5bb28a9f5aeba8d2f01bf5332d7af44544c
janoamelioortiz <84166100+janoamelioortiz@users.noreply.github.com>
2021-06-09 20:28:46 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBar(){
+    return (
+        <div style={{width: '100%', height: '40px', background: 'lightblue'}}>
+         <h1>NavBar</h1>
+        </div>
+
+    )
+
+}
+
+export default NavBar;
